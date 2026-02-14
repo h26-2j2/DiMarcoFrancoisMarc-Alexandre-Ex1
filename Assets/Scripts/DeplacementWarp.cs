7cbaf883a7aecf1ac43673227a74369fac21eb20
dimarcoc88@gmail.com <dimarcoc88@gmail.com>
2026-02-14 12:21:25 -0500

    explication de la logique de l'hexagone et remise de l'exercice

--- Assets/Scripts/DeplacementWarp.cs
+++ Assets/Scripts/DeplacementWarp.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class DeplacementWarp : MonoBehaviour
 {
+    // Variables publiques
     public bool cheminInversee;
     public float vitesseY;
     public float limiteHaute;
     public float limiteBasse;
 
+    // Variables privés
     private float positionX;
     private bool downScale = false;
     private float scale = 1f;
@@ -14,9 +16,11 @@ public class DeplacementWarp : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Valeurs aléatoires
         positionX = Random.Range(-2f, 2f);
         vitesseY = Random.Range(1.5f, 4f);
 
+        // Assignation de la valeur aléatoire positionX au composant Transform du gameobject.
         Transform transformX = GetComponent<Transform>();
         transformX.Translate(positionX, 0, 0);
     }
@@ -28,7 +32,8 @@ public class DeplacementWarp : MonoBehaviour
         // logique de déplacement
         positionX = transform.position.x;
         
-
+        // cheminInverse (true) => chemin hexagone haut vers le bas.
+        // cheminInverse (false) => chemin hexagone bas vers le haut.
         if (cheminInversee)
         {
             transform.position -= new Vector3(0, vitesseY, 0) * Time.deltaTime;
@@ -49,23 +54,22 @@ public class DeplacementWarp : MonoBehaviour
         }
 
         // logique de l'échelle
-
         if (transform.localScale.x >= 4f)
         {
-            downScale = true;
+            downScale = true; // déclare que l'échelle doit diminuer
             transform.localScale -= new Vector3(scale, scale, 0) * Time.deltaTime;
         }
-        else if (transform.localScale.x >= 1f && downScale)
+        else if (transform.localScale.x >= 1f && downScale) // si l'échelle doit diminuer, descend le scale
         {
             transform.localScale -= new Vector3(scale, scale, 0) * Time.deltaTime;
         }
-        else if (transform.localScale.x <= 4f && !downScale)
+        else if (transform.localScale.x <= 4f && !downScale) // si l'échelle ne doit pas diminuer, augmente le scale
         {
             transform.localScale += new Vector3(scale, scale, 0) * Time.deltaTime;
         }
         else if (transform.localScale.x <= 1f)
         {
-            downScale = false;
+            downScale = false; // déclare que l'échelle doit augmenter
             transform.localScale += new Vector3(scale, scale, 0) * Time.deltaTime;
         }
     }
